fbea060c7bd755238fec90c8a5d5357196596e4b
yixin <yingxinl@foxmail.com>
2017-06-18 22:23:01 +1200

    add signup page
    add signup page

--- very new/js/SignupScript.js
+++ very new/js/SignupScript.js
@@ -4,12 +4,16 @@ function checkform(){
   var name = document.getElementById("name").value;
   var surname = document.getElementById("surname").value;
   var email = document.getElementById("email").value;
+  var date = document.getElementById("date").value;
   var check = document.getElementById("check").checked;
+
   var namecheck = /^[a-zA-Z]{3,}$/;
   var surnamecheck = /^[a-zA-Z]{5,}$/;
   var emailcheck = /^([a-zA-Z0-9-_]+)(@[a-zA-Z0-9-_]+).([a-zA-Z0-9-_]+)/;
+  var datecheck = /^([0-9]+)-([0-9]+)-([0-9]+)/;
 
-  if(name == "" || name == null || surname == "" || surname == null || email == "" || email == null){
+
+  if(name == "" || name == null || surname == "" || surname == null || email == "" || email == null || date == "" || date == null){
     alert("Form Not Completed!");
     return false;
   }else if (check != true) {
@@ -24,7 +28,8 @@ function checkform(){
   }else if (!emailcheck.test(email)){
     alert("Email format must be correct");
     return false;
-  }else {
+  }else  {
+    alert("Sign Up successfully");
     return true;
   }
 }
